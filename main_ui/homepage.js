21b8a6e0e31296c1e971a21f1b69a65e63acb465
Kaustav Bhattacharya <kaustavb79@gmail.com>
2022-10-02 20:09:03 +0530

    - class java

--- main_ui/homepage.js
+++ main_ui/homepage.js
@@ -3,24 +3,32 @@
 function getConfig(json_file){
     $.getJSON(json_file, function(json) {        
         initTabs(json);
+        $("#run").click(function(){
+            alert("Submitted");
+        });
     });
 }
 
-getConfig("lang_config.json");
+$(document).ready(function(){
+    getConfig("lang_config.json");
+});
+
 
 function initTabs(lang_config){
-    console.log(lang_config);
+    // console.log(lang_config);
     var count = 1;
     for (let key in lang_config) {
-        console.log(key, lang_config[key]);
+        // console.log(key, lang_config[key]);
         var btn_id = lang_config[key]+"-tab";
         var btn_target_id = "editor-"+lang_config[key];
         var btn_extension = lang_config[key];
         var btn_class = "nav-link pe-3";
+        var aria_selected = false;
         if(count === 1){
             btn_class += " active";
+            aria_selected = true;
         }
-        var text = "<button class='"+btn_class+"' id='"+btn_id+"' data-bs-toggle='tab' data-bs-target='#"+btn_target_id+"'   type='button' role='tab' aria-controls='"+btn_extension+"' aria-selected='true'>"+key+"</button>"
+        var text = "<button class='"+btn_class+"' id='"+btn_id+"' data-bs-toggle='tab' data-bs-target='#"+btn_target_id+"'   type='button' role='tab' aria-controls='"+btn_extension+"' aria-selected='"+aria_selected+"'>"+key+"</button>"
         
         $("#v-pills-tab").append(text);
 
@@ -30,6 +38,11 @@ function initTabs(lang_config){
 }
 
 function updateEditor(lang,id,target_id,extension){
+    var class_name_input = ""
+    if(lang === "java"){
+
+    }
+
     var editor_section = `<div class="tab-pane fade editor-segment" id="`+target_id+`" role="tabpanel" aria-labelledby="`+id+`">
         <div class="row">
             <div class="col">
@@ -58,3 +71,5 @@ function updateEditor(lang,id,target_id,extension){
 
     $(".tab-content").append(editor_section);
 }
+
+
