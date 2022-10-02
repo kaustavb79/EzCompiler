9a0306a13b4dbe88a0b154a466fcb94f42ac92d0
Kaustav Bhattacharya <kaustavb79@gmail.com>
2022-10-02 15:53:48 +0530

    - ui fix

--- main_ui/homepage.js
+++ main_ui/homepage.js
@@ -1,2 +1,12 @@
-// $(document).ready(function() {
-// });
+$(document).ready(function() {
+    init();
+});
+
+function init(){
+    $("#v-pills-tab .nav-link").unbind("click.selected-tab",tabClick)
+    $("#v-pills-tab .nav-link").bind("click.selected-tab",tabClick)
+};
+
+function tabClick(this){
+    console.info($(this).attr("aria-controls"))
+}
